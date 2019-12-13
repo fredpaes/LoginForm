541df0c5b244f150bd3b8c32a72276bd462ef432
Fred Paucar <fred.paucarespinoza@gmail.com>
2019-12-13 14:54:10 -0500

    adding darkmode

---
+++ script.js
@@ -0,0 +1,16 @@
+const options = {
+  bottom: '64px', // default: '32px'
+  right: 'unset', // default: '32px'
+  left: '32px', // default: 'unset'
+  time: '0.3s', // default: '0.3s'
+  mixColor: '#fff', // default: '#fff'
+  backgroundColor: '#1a1c2f',  // default: '#fff'
+  buttonColorDark: '#fff',  // default: '#100f2c'
+  buttonColorLight: '#1a1c2f', // default: '#fff'
+  saveInCookies: false, // default: true,
+  label: '🌓', // default: ''
+  autoMatchOsTheme: true // default: true
+}
+
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
